a964dedec0a033f85664addfec139f0c97b302bb
Luis Romero <v.luis.romero@tropicana.com>
2025-04-07 13:53:05 +0200

    🚀 Proyecto inicial limpio

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Radar de Anomalías Espaciales",
+  description: "Monitoreo de objetos y señales en el espacio en tiempo real.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
